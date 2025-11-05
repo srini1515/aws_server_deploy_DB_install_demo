cde9f46a4c9d562200c69d634efda16c4719c461
srini1515 <srinivasanit1515@gmail.com>
2025-11-06 01:15:33 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "free_tier_linux" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"  
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Free-Tier-Linux"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl enable httpd
+              systemctl start httpd
+              echo "Hello from Terraform + AWS Free Tier!" > /var/www/html/index.html
+              EOF
+}
+
+output "public_ip" {
+  value = aws_instance.free_tier_linux.public_ip
+}
